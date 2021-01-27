e100032f4e70a358844a27c43b5fe8d311450dd6
Rafal Pajdak <rpajdak@gmail.com>
2021-01-27 18:36:26 +0100

    Create ReversalsToSort class.

---
+++ src/main/java/com/company/ReversalsToSort.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.*;
+
+public class ReversalsToSort {
+
+    /**
+     * You need to sort an array of integers by repeatedly reversing
+     * the order of the first several elements of it.
+     * <p>
+     * For example, to sort [11,14,12,13], you need to  reverse the order of the first two (2)
+     * elements and get [14,11,12,13], then reverse the order of the first four (4) elements
+     * and get [13,12,11,14] and then reverse the order of the first three (3) elements.
+     * <p>
+     * <p>
+     * The method should return array of integers corresponding to the required reversals.
+     * For the previous example, given an array [11,14,12,13]
+     * the method should return a array with integers [2,4,3].
+     */
+    public static List<Integer> getReversalsToSort(int[] array) {
+
+        return new ArrayList<>();
+    }
+}
