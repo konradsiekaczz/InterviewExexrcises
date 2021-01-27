808d4ef3b56e931727363423929999f8e2ca1f3b
Rafal Pajdak <rpajdak@gmail.com>
2021-01-27 18:32:21 +0100

    Create ClosestToZero class.

---
+++ src/main/java/com/company/ClosestToZero.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class ClosestToZero {
+
+    /**
+     * Please implement this method to
+     * return the number in the array that is closest to zero.
+     * If there are two equally close to zero elements like 2 and -2
+     * - consider the positive element to be "closer" to zero.
+     */
+    public int getClosestToZero(int[] a) {
+        return -1;
+    }
+
+}
+
