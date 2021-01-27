e3daf5c6903882c7bc00d9ea4720fcd5adf6ca30
Rafal Pajdak <rpajdak@gmail.com>
2021-01-27 18:33:48 +0100

    Create CountChunks class.

---
+++ src/main/java/com/company/CountChunks.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class CountChunks {
+
+    /**
+     * Please implement this method to return count of chunks in given array.
+     * <p>
+     * Chunk is defined as continous sequence of one or more numbers separated by one or more zeroes.
+     * Array can contain leading and trailing zeroes.
+     * <p>
+     * Example: array [5, 4, 0, 0, -1, 0, 2, 0, 0] contains 3 chunks
+     */
+    public static int countChunks(int[] array) {
+        return -1;
+    }
+
+}
